e9b43b751e8d6d7cc98d9c9e242b437dd0381b85
v.shirokova <v.shirokova@tinkoff.ru>
2023-08-03 15:20:49 +0300

    Added view binding for the second activity

--- app/src/main/java/ru/tinkoff/application/CatActivity.kt
+++ app/src/main/java/ru/tinkoff/application/CatActivity.kt
@@ -2,10 +2,14 @@ package ru.tinkoff.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.tinkoff.application.databinding.ActivityCatBinding
 
 class CatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cat)
+        binding = ActivityCatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

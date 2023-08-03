20419da3a4e4a60aba74fc44da2d02df8c29a61e
v.shirokova <v.shirokova@tinkoff.ru>
2023-08-03 15:16:17 +0300

    Added a second Activity

--- app/src/main/java/ru/tinkoff/application/MainActivity.kt
+++ app/src/main/java/ru/tinkoff/application/MainActivity.kt
@@ -1,11 +1,22 @@
 package ru.tinkoff.application
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.tinkoff.application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val button = binding.catButton
+        button.setOnClickListener {
+            val intent = Intent(this, CatActivity::class.java)
+            startActivity(intent)
+        }
     }
-}g
+}
